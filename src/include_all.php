517cfdb00b7505fce359a5027644bcf365ca1eeb
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-01 02:31:44 +0300

    .

--- src/include_all.php
+++ src/include_all.php
@@ -1,8 +1,8 @@
 <?php
 
-$env = (getenv('APP_ENV') ?: $_ENV['APP_ENV'] ?? '') ?? 'development';
+$env = getenv('APP_ENV') ?: ($_ENV['APP_ENV'] ?? 'development');
 
-if ($env === 'development') {
+if ($env === 'development' && file_exists(__DIR__ . '/load_env.php')) {
     include_once __DIR__ . '/load_env.php';
 }
 include_once __DIR__ . '/api_cod/helps.php';
